75bd953a72b0259e98fcbf613a0a77c63d8fe465
MariGenovart <genovart.mariajose@gmail.com>
2026-06-19 20:50:58 -0300

    Agrego la pantalla de inicio

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,9 +13,42 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return const MainLayout(
       title: 'Inicio',
-      body: Center(
-        child: Text('¡Bienvenido al inicio de la App!'),
-      ),
+      body: _listaNovedades(),
     );
   }
+  Widget _listaNovedades(){
+    return FutureBuilder(
+      future: novedadesProvider.cargarData(), 
+      initialData: const [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        
+        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          final data = snapshot.data!;
+
+          return ListView.builder(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            itemCount: data.length, 
+            itemBuilder: (context, index) {
+              final novedad = data[index]; 
+              
+              return TarjetaInicio(
+                titulo: novedad['titulo'],
+                descripcion: novedad['descripcion'],
+                urlImagen: novedad['imagen'], 
+              );
+            },
+          );
+        } else {
+          return const Center(child: Text('No hay novedades disponibles en este momento.'));
+        }
+      },
+    );
+  }
+
+
+
 }
